8afeddd16401552144ec29404baba89384fabeb9
Estevam <estevamfurtado@gmail.com>
2022-03-16 09:47:54 -0300

    Reset + decks and flashcards data structure

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+
+export default function App(props) {
+    
+    return (
+        <div></div>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+
+const decks = [
+    {title: "Deck 1", flashcards: [
+        {question: "O que é JSX?", answer: "Uma extensão de linguagem do JavaScript"},
+        {question: "O React é __", answer: "uma biblioteca JavaScript para construção de interfaces"},
+        {question: "Componentes devem iniciar com __", answer: "letra maiúscula"},
+        {question: "Podemos colocar __ dentro do JSX", answer: "expressões"},
+        {question: "O ReactDOM nos ajuda __", answer: "interagindo com a DOM para colocar componentes React na mesma"},
+        {question: "Usamos o npm para __", answer: "gerenciar os pacotes necessários e suas dependências"},
+        {question: "Usamos props para __", answer: "passar diferentes informações para componentes"} ,
+        {question: "Usamos estado (state) para __", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}
+    ]},
+    {title: "Deck 2", flashcards: [
+        {question: "O que é JSX?", answer: "Uma extensão de linguagem do JavaScript"},
+        {question: "O React é __", answer: "uma biblioteca JavaScript para construção de interfaces"},
+        {question: "Componentes devem iniciar com __", answer: "letra maiúscula"},
+        {question: "Usamos estado (state) para __", answer: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}
+    ]}
+]
+
+
+
+ReactDOM.render(
+    <App decks={decks}/>,
+    document.querySelector(".root")
+)
